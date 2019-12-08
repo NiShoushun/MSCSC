fa608e51977c2d5239d27d1210ec31953bca85ea
倪守顺 <Ni18724787072@outlook.com>
2019-12-08 10:20:46 +0800

    修改了sql文件

--- Sql脚本/创建数据库.sql
+++ Sql脚本/创建数据库.sql
@@ -1,29 +1,11 @@
-use 学生管理系统
-
---创建数据库
-create database MSCSA
-on primary(
-	Name = MSCSA_DATA,
-	FILEName = 'E:\Microsoft SQL Server\DATA\MSCSA.mdf',
-	SIZE = 8MB,
-	MAXSIZE = 20MB,
-	FILEGrowth = 10%
-),
-(
-	NAME = MSCSA_DATA_2,
-	FILENAME = 'E:\Microsoft SQL Server\DATA\MSCSA2.ndf',
-	SIZE = 5MB,
-	maxsize = 10MB,
-	FILEGrowth = 10%
-)
-LOG on
-(
-	Name = 学生管理系统_LOG,
-	FILENAME = 'E:\Microsoft SQL Server\LOG\MSCSA.ldf',
-	SIZE = 8mb,
-	MAXSIZE = 20MB,
-	FILEGROWTH = 5%
-)
-
+/****** Object:  Database [MSCSA]    Script Date: 2019/12/8 10:19:25 ******/
+CREATE DATABASE [MSCSA]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'MSCSA_DATA', FILENAME = N'E:\Microsoft SQL Server\DATA\MSCSA.mdf' , SIZE = 8192KB , MAXSIZE = 20480KB , FILEGROWTH = 10%),
+( NAME = N'MSCSA_DATA_2', FILENAME = N'E:\Microsoft SQL Server\DATA\MSCSA2.ndf' , SIZE = 5120KB , MAXSIZE = 10240KB , FILEGROWTH = 10%)
+ LOG ON 
+( NAME = N'瀛︾敓绠＄悊绯荤粺_LOG', FILENAME = N'E:\Microsoft SQL Server\LOG\MSCSA.ldf' , SIZE = 8192KB , MAXSIZE = 20480KB , FILEGROWTH = 5%)
+GO
 
 
--- Sql脚本/表.sql
+++ Sql脚本/表.sql
@@ -1,72 +1,69 @@
-	use MSCSA
-go
+use MSCSA
 
-create table 学生
-(
-	学号 nvarchar(10) not NULL,
-	姓名 nvarchar(10) not NULL, 
-	年龄 int not NULL,
-	性别 nvarchar(2) not NULL,
-	院系 nvarchar(20) not NULL,
-	年级 int not NULL,
-	电话号码 nvarchar(12)
-)
+CREATE TABLE [dbo].[学生](
+	[学号] [nvarchar](10) NOT NULL,
+	[姓名] [nvarchar](10) NOT NULL,
+	[年龄] [int] NOT NULL,
+	[性别] [nvarchar](2) NOT NULL,
+	[院系] [nvarchar](20) NOT NULL,
+	[年级] [int] NOT NULL,
+	[电话号码] [nvarchar](12) NULL
+) ON [PRIMARY]
 
-create table 社团
-(
-	社团ID nvarchar(10) not NULL,
-	社团名称 nvarchar(20) not NULL,
-	社团创始人 nvarchar(10) not NULL,
-	社团简介 text,
-	成立日期 datetime not NULL,
-	当前人数 int not NULL,
-	最大人数 int not NULL,
-)
 
-create table 待审核社团
-(
-	社团名称 nvarchar(20) not NULL,
-	社团创始人 nvarchar(10) not NULL,
-	社团简介 text,
-	申请日期 datetime not NULL,
-	是否通过 bit
-)
+CREATE TABLE [dbo].[社团](
+	[社团ID] [nvarchar](10) NOT NULL,
+	[社团名称] [nvarchar](20) NOT NULL,
+	[社团创始人] [nvarchar](10) NOT NULL,
+	[社团简介] [text] NULL,
+	[成立日期] [datetime] NOT NULL,
+	[当前人数] [int] NOT NULL,
+	[最大人数] [int] NOT NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[待审核社团](
+	[社团名称] [nvarchar](20) NOT NULL,
+	[社团创始人] [nvarchar](10) NOT NULL,
+	[社团简介] [text] NULL,
+	[申请日期] [datetime] NOT NULL,
+	[是否通过] [bit] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 --insert into 待审核社团 values('1','1','简介',getDate(),1)
 --select convert(char(10),申请日期,120) from 待审核社团
 --delete from 待审核社团
 
-create table 社团成员
-(
-	学号 nvarchar(10) not NULL,
-	社团ID nvarchar(10) not NULL,
-	成员ID nvarchar(10) not NULL,
-	加入时间 datetime not NUll,
-	权限 nvarchar(5) not NULL
-)
 
-create table 退会成员
-(
-	学号 nvarchar(10) not NULL,
-	社团ID nvarchar(10) not NULL,
-	退出时间 datetime not NUll,
-	联系方式 text
-)
+CREATE TABLE [dbo].[社团成员](
+	[学号] [nvarchar](10) NOT NULL,
+	[社团ID] [nvarchar](10) NOT NULL,
+	[成员ID] [nvarchar](10) NOT NULL,
+	[加入时间] [datetime] NOT NULL,
+	[权限] [nvarchar](5) NOT NULL
+) ON [PRIMARY]
 
-create table 登陆表
-(
-	学号 nvarchar(10) not NULL,
-	密码 nvarchar(32) not NULL
-)
 
-create table 待审核社团成员
-(
-	学号 nvarchar(10) not NULL,
-	社团ID nvarchar(10) not NULL,
-	申请时间 datetime not NUll,
-	是否通过 bit not NULL
-)
+CREATE TABLE [dbo].[退会成员](
+	[学号] [nvarchar](10) NOT NULL,
+	[社团ID] [nvarchar](10) NOT NULL,
+	[退出时间] [datetime] NOT NULL,
+	[联系方式] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
-alter table 待审核社团成员 add 是否通过 bit not NULL
 
-go
+CREATE TABLE [dbo].[待审核社团成员](
+	[学号] [nvarchar](10) NOT NULL,
+	[社团ID] [nvarchar](10) NOT NULL,
+	[申请时间] [datetime] NOT NULL,
+	[是否通过] [bit] NOT NULL,
+	[加入说明] [text] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+CREATE TABLE [dbo].[登陆表](
+	[学号] [nvarchar](10) NOT NULL,
+	[密码] [nvarchar](32) NOT NULL
+) ON [PRIMARY]
+
+
