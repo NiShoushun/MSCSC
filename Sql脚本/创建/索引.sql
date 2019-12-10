bc138a2fd045547ae20462f787899550da33c543
倪守顺 <Ni18724787072@outlook.com>
2019-12-10 09:39:02 +0800

    加入索引 查看袭击信息功能

---
+++ Sql脚本/创建/索引.sql
@@ -0,0 +1,6 @@
+USE MSCSC
+
+--在学生表中使用学号创建唯一性索引
+
+CREATE UNIQUE INDEX Index_Stu
+ON 学生(学号)
--- Sql脚本/功能/学生基本功能/查看社团信息.sql
+++ Sql脚本/功能/学生基本功能/查看社团信息.sql
@@ -1,4 +1,4 @@
---学生查看社团信息功能
+--学生查看特定社团信息功能
 use MSCSC
 go
 
@@ -12,11 +12,30 @@ BEGIN
     Where 社团ID = @ComID AND 学生.学号 = 社团成员.学号
 END
 
+
 go
 
+
+--学生查看所有社团信息功能
 Create Proc ViewAllComInfo
 @ComID nvarchar(10)
 AS 
 BEGIN
     SELECT 社团ID,社团名称,成立日期,当前人数 from 社团 Where 社团ID = @ComID
+END
+
+
+GO
+
+--学生查看自己的信息
+CREATE PROC ShowSelfInfo
+@StuNO NVARCHAR(10)
+AS 
+BEGIN
+    --选择学生信息
+    SELECT * FROM 学生
+    --选择所加入社团信息
+    SELECT 社团.社团ID,社团.社团ID ,社团成员.成员ID FROM 社团 ,社团成员 
+    WHERE 社团成员.学号 = @StuNo 
+    AND 社团成员.社团ID = 社团.社团ID
 END
