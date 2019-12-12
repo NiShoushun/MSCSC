3deb39dc0046525b3b6799f4f8d10e5cba1f5df2
倪守顺 <Ni18724787072@outlook.com>
2019-12-12 09:50:07 +0800

    批准社团成立

--- Sql脚本/功能/社团管理员基本功能/批准社团成立.sql
+++ Sql脚本/功能/社团管理员基本功能/批准社团成立.sql
@@ -8,12 +8,12 @@ BEGIN
     DECLARE @Msg NVARCHAR(20)
     --首先检查是否存在该待审核社团
     DECLARE @Count INT
-    SELECT COUNT(*) FROM 待审核社团 WHERE 社团名称 = @ComName AND 是否通过 = 'False'
+    SELECT @Count = COUNT(*) FROM 待审核社团 WHERE 社团名称 = @ComName AND 是否通过 = 'False'
     --如果存在该待审核社团且未审核通过
-    IF @Conut !=0
+    IF @Count !=0
     BEGIN
         DECLARE @ComCreaterNo NVARCHAR(10) 
-        DECLARE @ComInfo TEXT
+        DECLARE @ComInfo NVARCHAR(1000)
         SELECT @ComCreaterNo = 社团创始人,@ComInfo = 社团简介 FROM 待审核社团 WHERE 社团名称 = @ComName AND 是否通过 = 'False'
         UPDATE 待审核社团 SET 是否通过 = 'True' WHERE 社团名称 = @ComName AND 是否通过 = 'False'
         --获得社团数量
@@ -25,7 +25,6 @@ BEGIN
         --返回信息
         SET @Msg = '社团添加成功'
         SELECT @Msg '系统信息'
-
     END
     ELSE 
     BEGIN
