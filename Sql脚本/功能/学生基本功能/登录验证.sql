98b3eb2470291b8ca4559fda4efa4f8c083d4552
倪守顺 <Ni18724787072@outlook.com>
2019-12-15 22:05:13 +0800

    修改MD5处理乱码问题

--- Sql脚本/功能/学生基本功能/登录验证.sql
+++ Sql脚本/功能/学生基本功能/登录验证.sql
@@ -2,12 +2,13 @@ use MSCSC
 go
 
 --用户输入学号+密码，将密码MD5处理后，查询是否有该记录
-CREATE PROC VerifyUser
+ALTER PROC VerifyUser
 @StuInfo NVARCHAR(12),
-@Password NVARCHAR(40)
+@Password VARCHAR(100)
 AS
 BEGIN
-    SELECT COUNT(*) FROM 登陆表 
-    WHERE 学号 = @StuInfo AND 密码 = HashBytes('MD5',@Password) 
+    DECLARE @Result BIT
+    SELECT @Result = COUNT(*) from 登陆表 Where 密码 = HASHBYTES('MD5',@Password)
+    SELECT @Result AS '验证结果'
 END
     
